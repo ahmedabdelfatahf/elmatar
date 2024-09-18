bf029eb8bf5ce16ff3056aaeb51d2c9e9b8029b0
ahmed helal <ahmedabdelfatahf@gmail.com>
2024-09-18 07:25:11 +0300

    this is the new changes task

--- wuzzufTestNG/src/test/java/Tests/homeTEST.java
+++ wuzzufTestNG/src/test/java/Tests/homeTEST.java
@@ -5,6 +5,7 @@ import Pages.homePage;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -12,12 +13,19 @@ import java.util.ArrayList;
 public class homeTEST extends BaseTest{
     homePage hp;
     careerInterestsPage cip;
+    String search="software testing";
     @Test
     public void tc1() {
+        reportTest=report.createTest("wuzzuf testcase");
+customSoftAssert(softAssert,driver.getCurrentUrl().equals(url),"this is the right website and it is "+url,"this is wrong url and the right is "+url);
 hp=new homePage(driver);
         cip=new careerInterestsPage(driver);
-hp.enterSearchData();
-hp.filter();
+hp.enterSearchData(search);
+customSoftAssert(softAssert,driver.getCurrentUrl().contains(search),"this is url before adding filter "+urlBeforeFilter,"this is the wrong url and the right is "+urlBeforeFilter);
+
+String searchInput=hp.filter();
+customSoftAssert(softAssert,searchInput.contains(search),"yes it contains software testing ","itt doesn't contain software testing ");
+customSoftAssert(softAssert,driver.getCurrentUrl().contains("within_1_week"),"this is url after adding filter "+urlAFTERFilter,"this is the wrong url and the right is "+urlAFTERFilter);
 System.out.println("the number of jobs  after filter is   "+ hp.jobsFound());
 hp.firstJob();
         WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -29,7 +37,7 @@ hp.signUp(firstName,lastName,email,password);
 cip.step1(salary);
 cip.step2(mobileNumber);
 cip.step3(fieldOFStudy,"a",SKILLS1,"C:\\Users\\pc\\Desktop\\cvAhmedAbdelFatah1.pdf",SKILLS2);
-cip.apply(salary);
+cip.apply();
 
 
 
